7c4fc9a0388da6fce2003b2563266328373348c0
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-09-12 12:00:25 -0300

    :feat: Criando componente Banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Header from './partials/Header';
+import Banner from './components/Banner';
 
 const App = () => {
   return(
     <>
       <Header />
+      <main>
+        <Banner />
+      </main>
     </>
   );
 }
---
+++ src/components/Banner.tsx
@@ -0,0 +1,17 @@
+import './Banner.scss';
+
+const Banner = () => {
+  return(
+    <div className='banner'>
+      <div className='shadow'/>
+      <h1 className='banner-title'>Venha conhecer as nossa promoções</h1>
+      <h2 className='banner-subtitle'>50% Off nos produos</h2>
+
+      <button className='banner-button'>
+        Ver produtos
+      </button>
+    </div>
+  );
+}
+
+export default Banner;
